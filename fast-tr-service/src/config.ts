8b5470e27622db83360b63d33e5ef0f3337109ac
SwineCoder101 <liamfaruq@gmail.com>
2023-09-24 06:02:09 -0400

    update-safe-transfers

--- fast-tr-service/src/config.ts
+++ fast-tr-service/src/config.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const Config = {
+  // 8395b92e4acc294baa248db634c745f94d91841990eec78f9590161f98f5181c
   port: parseInt(process.env.PORT || '8080'),
   rpcUrl: process.env.RPC_ALCHEMY_URL_GOERLI || 'https://eth-goerli.public.blastapi.io',
   txServiceUrl: process.env.TX_SERVICE_URL || 'https://safe-transaction-goerli.safe.global',
--- fast-tr-service/src/server.ts
+++ fast-tr-service/src/server.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import { walletRouter } from './routes/wallet.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON body
-app.use(express.json());
+// app.use(express.json());
+app.use(bodyParser.json());
 
 // Mount the router on the app
 app.use('/fast', walletRouter);
+// app.use(walletRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.use(walletRouter);
